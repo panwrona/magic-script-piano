daa2fc4d9271e45fb23f2295bc51460986f31f29
panwrona <mariusz.brona@gmail.com>
2019-12-09 17:11:55 -0500

    Remove unnecessary files and add semi keys

--- src_app/hackathon_app.js
+++ src_app/hackathon_app.js
@@ -124,6 +124,22 @@ class HackathonApp extends React.Component {
     )
   }
 
+  render_D() {
+    return (
+      <view>
+        <audio
+          fileName={require('../resources/d_3.wav')}
+          action={'start'}
+          soundLooping={false}
+          soundMute={false}
+          soundVolumeLinear={4.0}
+          spatialSoundEnable={true}
+          spatialSoundPosition={{ 'channel': 0, channelPosition: [0, 0, 0] }}
+        />
+      </view>
+    )
+  }
+
   renderE() {
     return (
       <view>
@@ -156,11 +172,43 @@ class HackathonApp extends React.Component {
     )
   }
 
+  render_F() {
+    return (
+      <view>
+        <audio
+          fileName={require('../resources/f_3.wav')}
+          action={'start'}
+          soundLooping={false}
+          soundMute={false}
+          soundVolumeLinear={4.0}
+          spatialSoundEnable={true}
+          spatialSoundPosition={{ 'channel': 0, channelPosition: [0, 0, 0] }}
+        />
+      </view>
+    )
+  }
+
   renderG() {
     return (
       <view>
         <audio
           fileName={require('../resources/g3.wav')}
+          action={'start'}
+          soundLooping={false}
+          soundMute={false}
+          soundVolumeLinear={4.0}
+          spatialSoundEnable={true}
+          spatialSoundPosition={{ 'channel': 0, channelPosition: [0, 0, 0] }}
+        />
+      </view>
+    )
+  }
+
+  render_G() {
+    return (
+      <view>
+        <audio
+          fileName={require('../resources/g_3.wav')}
           action={'start'}
           soundLooping={false}
           soundMute={false}
@@ -188,6 +236,22 @@ class HackathonApp extends React.Component {
     )
   }
 
+  render_A() {
+    return (
+      <view>
+        <audio
+          fileName={require('../resources/a_3.wav')}
+          action={'start'}
+          soundLooping={false}
+          soundMute={false}
+          soundVolumeLinear={4.0}
+          spatialSoundEnable={true}
+          spatialSoundPosition={{ 'channel': 0, channelPosition: [0, 0, 0] }}
+        />
+      </view>
+    )
+  }
+
 
   onButtonClick(param) {
     return () => {
@@ -203,28 +267,32 @@ class HackathonApp extends React.Component {
 
 
   render() {
-    const { playC,play_C, playD, playE, playF, playG, playA } = this.state
+    const { playC,play_C, playD, play_D, playE, playF, play_F, playG, play_G, playA, play_A } = this.state
     return (
       <View name='main-view' alignment={'center-center'}>
         {this.renderNormalKey({ localPosition: [0, 0, 0], title: 'C',onClick: this.onButtonClick2({ playC: false },{ playC: true }) })}
-        {this.renderSpecialKey({ localPosition: [0.024, 0.075, 0], title: '', onClick: this.onButtonClick2({ play_C: false },{ play_C: true }) })}
+        {this.renderSpecialKey({ localPosition: [0.024, 0.075, 0], title: 'C#', onClick: this.onButtonClick2({ play_C: false },{ play_C: true }) })}
         {this.renderNormalKey({ localPosition: [0.048, 0, 0], title: 'D', onClick: this.onButtonClick2({ playD: false },{ playD: true }) })}
-        {this.renderSpecialKey({ localPosition: [0.072, 0.075, 0], title: '', onClick: this.onButtonClick({ title: '' }) })}
+        {this.renderSpecialKey({ localPosition: [0.072, 0.075, 0], title: 'D#', onClick: this.onButtonClick2({ play_D: false },{ play_D: true }) })}
         {this.renderNormalKey({ localPosition: [0.096, 0, 0], title: 'E', onClick: this.onButtonClick2({ playE: false },{ playE: true }) })}
         {this.renderNormalKey({ localPosition: [0.144, 0, 0], title: 'F', onClick: this.onButtonClick2({ playF: false },{ playF: true }) })}
-        {this.renderSpecialKey({ localPosition: [0.168, 0.075, 0], title: '', onClick: this.onButtonClick({ title: '' }) })}
+        {this.renderSpecialKey({ localPosition: [0.168, 0.075, 0], title: 'F#', onClick: this.onButtonClick2({ play_F: false },{ play_F: true }) })}
         {this.renderNormalKey({ localPosition: [0.192, 0, 0], title: 'G', onClick: this.onButtonClick2({ playG: false },{ playG: true }) })}
-        {this.renderSpecialKey({ localPosition: [0.216, 0.075, 0], title: '', onClick: this.onButtonClick({ title: '' }) })}
+        {this.renderSpecialKey({ localPosition: [0.216, 0.075, 0], title: 'G#', onClick: this.onButtonClick2({ play_G: false },{ play_G: true }) })}
         {this.renderNormalKey({ localPosition: [0.240, 0, 0], title: 'A', onClick: this.onButtonClick2({ playA: false },{ playA: true }) })}
-        {this.renderSpecialKey({ localPosition: [0.264, 0.075, 0], title: '', onClick: this.onButtonClick({ title: '' }) })}
+        {this.renderSpecialKey({ localPosition: [0.264, 0.075, 0], title: 'A#', onClick: this.onButtonClick2({ play_A: false },{ play_A: true }) })}
         {this.renderNormalKey({ localPosition: [0.288, 0, 0], title: 'H', onClick: this.onButtonClick({ title: 'H' }) })}
         {playC && this.renderC()}
         {play_C && this.render_C()}
         {playD && this.renderD()}
+        {play_D && this.render_D()}
         {playE && this.renderE()}
         {playF && this.renderF()}
+        {play_F && this.render_F()}
         {playG && this.renderG()}
+        {play_G && this.render_G()}
         {playA && this.renderA()}
+        {play_A && this.render_A()}
       </View>
     );
   }
